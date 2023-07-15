ad958a24af074ac106ebc2809d9405880164ca9e
hornetg30@gmail.com <hornetg30@gmail.com>
2023-07-15 19:20:22 +0530

    Insertion algo added

---
+++ Sorting/Insertion_sort.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr, n):
+    for i in range(1, n):
+        key = arr[i]
+        j = i-1
+        while j >=0 and arr[j] > key:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+
+    return arr
+
+# Driver code
+
+arr = [5, 2, 7, 1, 8]
+print(insertion_sort(arr, len(arr)))
