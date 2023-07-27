69294f6342580d0e2ae17e645870dbd1278a7195
hornetg30@gmail.com <hornetg30@gmail.com>
2023-07-27 16:56:49 +0530

    Merge Sort update

--- Sorting/Insertion_sort.py
+++ Sorting/Insertion_sort.py
@@ -1,15 +1,16 @@
 def insertion_sort(arr, n):
     for i in range(1, n):
         key = arr[i]
-        j = i-1
-        while j >=0 and arr[j] > key:
-            arr[j+1] = arr[j]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
             j -= 1
-        arr[j+1] = key
+        arr[j + 1] = key
 
     return arr
 
+
 # Driver code
 
-arr = [5, 2, 7, 1, 8]
+arr = [4, 3, 2, 10, 12, 1, 5, 6]
 print(insertion_sort(arr, len(arr)))
--- Sorting/Merge_sort.py
+++ Sorting/Merge_sort.py
@@ -1,36 +1,38 @@
-def mergeSort(arr):
-    n = len(arr)
-    if n > 1:
-        mid = n//2
-        L = arr[:mid]
-        R = arr[mid:]
+def mergeSort(arr, L, R):
+    if L<R:
+        mid = (L+R)// 2
+        mergeSort(arr, L, mid)
+        mergeSort(arr, mid + 1, R)
+        merge(arr, L, mid, R)
 
-        mergeSort(L)
-        mergeSort(R)
 
-        # i = j = k = 0
-        # while i < len(L) and j < len(R):
-        #     if L[i] <= R[j]:
-        #         arr[k] = L[i]
-        #         i += 1
-        #     else:
-        #         arr[k] = R[j]
-        #         j += 1
-        #     k += 1
-        #
-        # while i < len(L):
-        #     arr[k] = L[i]
-        #     i += 1
-        #     k += 1
-        #
-        # while j < len(R):
-        #     arr[k] = R[j]
-        #     j += 1
-        #     k += 1
+def merge(arr, L, mid, R):
+    left = arr[L: mid + 1]
+    right = arr[mid + 1: R + 1]
+    i, j = 0, 0
+    k = L
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+            k += 1
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+
+    return arr
+
 
 arr = [5, 2, 7, 1, 8, 10, 3, 25, 12]
-mergeSort(arr)
+mergeSort(arr,0, len(arr)-1)
 print(arr)
-
-
-
